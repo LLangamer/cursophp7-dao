be6dbb33b31ba743cd86d91787630c6bf8c0bac7
Lucas Langamer <lucaslangamerdacruz@hotmail.com>
2019-01-03 19:27:33 -0200

    métodos de lista

--- index.php
+++ index.php
@@ -7,11 +7,33 @@ require_once("config.php");
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($usuarios);*/
 
-$root = new Usuario();
+//carrega 1 usuario
+//$root = new Usuario();
+//$root->loadById(3);
+//echo $root;
 
-$root->loadById(3);
 
-echo $root;
+//Carrega uma lista de usuarios
+
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+
+//Carrega uma lista de usuarios buscando pelo login
+
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+
+//carrega um usuario usando o login e senha
+
+$usuario = new Usuario();
+
+$usuario->login('jose','1234567890');
+
+echo $usuario;
+
+
 
 
  ?>
